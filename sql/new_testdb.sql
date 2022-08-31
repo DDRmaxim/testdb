5ea7e207e9a2f6c895494ba004eedb8998a7215b
DDRmaxim <ddrmaxim@gmail.com>
2022-08-31 18:22:06 +0300

    add dump db

--- sql/new_testdb.sql
+++ sql/new_testdb.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Хост: db
--- Время создания: Авг 30 2022 г., 22:49
--- Версия сервера: 8.0.30
--- Версия PHP: 8.0.19
+-- Хост: 127.0.0.1:3306
+-- Время создания: Авг 31 2022 г., 18:19
+-- Версия сервера: 5.6.51
+-- Версия PHP: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,18 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `NL_HOUSES`
+--
+
+CREATE TABLE `NL_HOUSES` (
+  `ID_NL_HOUSES` int(11) NOT NULL,
+  `ID_NL_MATERIAL` int(11) DEFAULT NULL,
+  `NL_HOUSES_SHORT` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `NL_HOUSES`
+--
+
+INSERT INTO `NL_HOUSES` (`ID_NL_HOUSES`, `ID_NL_MATERIAL`, `NL_HOUSES_SHORT`) VALUES
+(1, 1, 'Железобетонный'),
+(2, 2, 'Блочный'),
+(3, 3, 'Кирпичный');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `NL_LOG`
 --
 
 CREATE TABLE `NL_LOG` (
-  `ID_NL_LOG` int NOT NULL,
+  `ID_NL_LOG` int(11) NOT NULL,
   `NL_LOG_DATE` date NOT NULL,
   `NL_LOG_TIME` time NOT NULL,
-  `NL_LOG_IP` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `NL_LOG_IUD` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `NL_LOG_TABLE_NAME` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `ID_NL_USER` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `NL_LOG_IP` varchar(255) NOT NULL,
+  `NL_LOG_IUD` varchar(255) NOT NULL,
+  `NL_LOG_TABLE_NAME` varchar(255) NOT NULL,
+  `ID_NL_USER` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `NL_LOG`
+--
+
+INSERT INTO `NL_LOG` (`ID_NL_LOG`, `NL_LOG_DATE`, `NL_LOG_TIME`, `NL_LOG_IP`, `NL_LOG_IUD`, `NL_LOG_TABLE_NAME`, `ID_NL_USER`) VALUES
+(1, '2022-08-31', '13:51:23', '127.0.0.1', 'edit', 'NL_PROP_RESALE', 2),
+(2, '2022-08-31', '13:54:25', '127.0.0.1', 'edit', 'NL_HOUSES', 2),
+(3, '2022-08-31', '13:54:30', '127.0.0.1', 'edit', 'NL_HOUSES', 2),
+(4, '2022-08-31', '13:54:39', '127.0.0.1', 'edit', 'NL_HOUSES', 2),
+(5, '2022-08-31', '17:57:05', '127.0.0.1', 'add', 'NL_PROP_RESALE', 2);
 
 -- --------------------------------------------------------
 
@@ -44,12 +76,73 @@ CREATE TABLE `NL_LOG` (
 --
 
 CREATE TABLE `NL_LOG_DETAIL` (
-  `ID_NL_LOG_DETAIL` int NOT NULL,
-  `ID_NL_LOG` int NOT NULL,
-  `NL_LOG_DETAIL_OLD` varchar(2550) COLLATE utf8mb4_general_ci NOT NULL,
-  `NL_LOG_DETAIL_NEW` varchar(2550) COLLATE utf8mb4_general_ci NOT NULL,
-  `NL_LOG_DETAIL_FIELD` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID_NL_LOG_DETAIL` int(11) NOT NULL,
+  `ID_NL_LOG` int(11) NOT NULL,
+  `NL_LOG_DETAIL_OLD` varchar(2550) NOT NULL,
+  `NL_LOG_DETAIL_NEW` varchar(2550) NOT NULL,
+  `NL_LOG_DETAIL_FIELD` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `NL_LOG_DETAIL`
+--
+
+INSERT INTO `NL_LOG_DETAIL` (`ID_NL_LOG_DETAIL`, `ID_NL_LOG`, `NL_LOG_DETAIL_OLD`, `NL_LOG_DETAIL_NEW`, `NL_LOG_DETAIL_FIELD`) VALUES
+(1, 1, '3', '3', 'ID_NL_PROP_RESALE'),
+(2, 1, '555.00', '555', 'NL_PROP_RESALE_AREA_FULL'),
+(3, 1, 'Россия, Краснодарский край, Анапа, Советская улица ', 'Россия, Краснодарский край, Анапа, Советская улица ', 'NL_PROP_RESALE_ADDRESS'),
+(4, 1, '1', '1', 'NL_PROP_RESALE_FLOOR'),
+(5, 1, '1000000', '1000000', 'NL_PROP_RESALE_COST_TOTAL'),
+(6, 1, '', '', 'NL_PROP_RESALE_PHONE_OWNER'),
+(7, 1, '', '1', 'ID_NL_VIEW'),
+(8, 1, '', '2', 'ID_NL_HOUSES'),
+(9, 1, '', '2', 'ID_NL_MATERIAL'),
+(10, 1, '1', '1', 'ID_NL_USER'),
+(11, 1, '+79282601474', '+79282601474', 'NL_PROP_RESALE_PHONE'),
+(12, 1, '[\"/img/prop_resale/PHOTO_URLS_3_191201_024304.jpg\"]', '[\"/img/prop_resale/PHOTO_URLS_3_191201_024304.jpg\"]', 'NL_PROP_RESALE_PHOTO_URLS'),
+(13, 1, '%7B%22ops%22%3A%5B%7B%22insert%22%3A%22%D0%A5%D0%BE%D1%80%D0%BE%D1%88%D0%B0%D1%8F%20%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D0%B0%5Cn%22%7D%5D%7D', '%7B%22ops%22%3A%5B%7B%22insert%22%3A%22%D0%A5%D0%BE%D1%80%D0%BE%D1%88%D0%B0%D1%8F%20%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D0%B0%5Cn%22%7D%5D%7D', 'NL_PROP_RESALE_DESCRIPTION'),
+(14, 2, '3', '3', 'ID_NL_HOUSES'),
+(15, 2, 'Кирпичный', 'Кирпичный', 'NL_HOUSES_SHORT'),
+(16, 2, '', '3', 'ID_NL_MATERIAL'),
+(17, 3, '2', '2', 'ID_NL_HOUSES'),
+(18, 3, 'Блочный', 'Блочный', 'NL_HOUSES_SHORT'),
+(19, 3, '', '2', 'ID_NL_MATERIAL'),
+(20, 4, '1', '1', 'ID_NL_HOUSES'),
+(21, 4, 'Железобетонный', 'Железобетонный', 'NL_HOUSES_SHORT'),
+(22, 4, '', '1', 'ID_NL_MATERIAL'),
+(23, 5, '', '7', 'ID_NL_PROP_RESALE'),
+(24, 5, '', '172', 'NL_PROP_RESALE_AREA_FULL'),
+(25, 5, '', 'Анапа, ул Авиагородок, 89', 'NL_PROP_RESALE_ADDRESS'),
+(26, 5, '', '3', 'NL_PROP_RESALE_FLOOR'),
+(27, 5, '', '4300000', 'NL_PROP_RESALE_COST_TOTAL'),
+(28, 5, '', 'нет', 'NL_PROP_RESALE_PHONE_OWNER'),
+(29, 5, '', '1', 'ID_NL_VIEW'),
+(30, 5, '', '2', 'ID_NL_HOUSES'),
+(31, 5, '', '2', 'ID_NL_MATERIAL'),
+(32, 5, '', '2', 'ID_NL_USER'),
+(33, 5, '', '89180295458', 'NL_PROP_RESALE_PHONE'),
+(34, 5, '', '[\"/img/prop_resale/PHOTO_URLS_7_220831_055646.png\"]', 'NL_PROP_RESALE_PHOTO_URLS'),
+(35, 5, '', '%7B%22ops%22%3A%5B%7B%22insert%22%3A%22%D0%9A%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D0%B0%20%D1%83%20%D0%BC%D0%BE%D1%80%D1%8F%5Cn%22%7D%5D%7D', 'NL_PROP_RESALE_DESCRIPTION');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `NL_MATERIAL`
+--
+
+CREATE TABLE `NL_MATERIAL` (
+  `ID_NL_MATERIAL` int(11) NOT NULL,
+  `NL_MATERIAL_SHORT` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `NL_MATERIAL`
+--
+
+INSERT INTO `NL_MATERIAL` (`ID_NL_MATERIAL`, `NL_MATERIAL_SHORT`) VALUES
+(1, 'Панель'),
+(2, 'Газоблок'),
+(3, 'Кирпич');
 
 -- --------------------------------------------------------
 
@@ -58,29 +151,30 @@ CREATE TABLE `NL_LOG_DETAIL` (
 --
 
 CREATE TABLE `NL_PROP_RESALE` (
-  `ID_NL_PROP_RESALE` int NOT NULL,
-  `ID_NL_VIEW` int DEFAULT NULL,
-  `ID_NL_HOUSES` int DEFAULT NULL,
-  `ID_NL_MATERIAL` int DEFAULT NULL,
-  `NL_PROP_RESALE_FLOOR` varchar(25) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `ID_NL_PROP_RESALE` int(11) NOT NULL,
+  `ID_NL_VIEW` int(11) DEFAULT NULL,
+  `ID_NL_HOUSES` int(11) DEFAULT NULL,
+  `ID_NL_MATERIAL` int(11) DEFAULT NULL,
+  `NL_PROP_RESALE_FLOOR` varchar(25) DEFAULT NULL,
   `NL_PROP_RESALE_AREA_FULL` decimal(6,2) NOT NULL,
-  `NL_PROP_RESALE_PHOTO_URLS` varchar(5100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `NL_PROP_RESALE_COST_TOTAL` int DEFAULT NULL,
-  `NL_PROP_RESALE_ADDRESS` varchar(2550) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `NL_PROP_RESALE_DESCRIPTION` varchar(5100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `ID_NL_USER` int DEFAULT NULL,
-  `NL_PROP_RESALE_PHONE` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `NL_PROP_RESALE_PHONE_OWNER` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `NL_PROP_RESALE_PHOTO_URLS` varchar(5100) DEFAULT NULL,
+  `NL_PROP_RESALE_COST_TOTAL` int(11) DEFAULT NULL,
+  `NL_PROP_RESALE_ADDRESS` varchar(2550) DEFAULT NULL,
+  `NL_PROP_RESALE_DESCRIPTION` varchar(5100) DEFAULT NULL,
+  `ID_NL_USER` int(11) DEFAULT NULL,
+  `NL_PROP_RESALE_PHONE` varchar(50) DEFAULT NULL,
+  `NL_PROP_RESALE_PHONE_OWNER` varchar(255) DEFAULT NULL,
   `NL_PROP_RESALE_DATE_INSERT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `NL_PROP_RESALE_DATE_UPDATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `NL_PROP_RESALE`
 --
 
 INSERT INTO `NL_PROP_RESALE` (`ID_NL_PROP_RESALE`, `ID_NL_VIEW`, `ID_NL_HOUSES`, `ID_NL_MATERIAL`, `NL_PROP_RESALE_FLOOR`, `NL_PROP_RESALE_AREA_FULL`, `NL_PROP_RESALE_PHOTO_URLS`, `NL_PROP_RESALE_COST_TOTAL`, `NL_PROP_RESALE_ADDRESS`, `NL_PROP_RESALE_DESCRIPTION`, `ID_NL_USER`, `NL_PROP_RESALE_PHONE`, `NL_PROP_RESALE_PHONE_OWNER`, `NL_PROP_RESALE_DATE_INSERT`, `NL_PROP_RESALE_DATE_UPDATE`) VALUES
-(3, NULL, NULL, NULL, '1', '555.00', '[\"/img/prop_resale/PHOTO_URLS_3_191201_024304.jpg\"]', 1000000, 'Россия, Краснодарский край, Анапа, Советская улица ', '%7B%22ops%22%3A%5B%7B%22insert%22%3A%22%D0%A5%D0%BE%D1%80%D0%BE%D1%88%D0%B0%D1%8F%20%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D0%B0%5Cn%22%7D%5D%7D', 1, '+79282601474', NULL, '2019-12-01 14:44:02', '2019-12-01 14:44:02');
+(3, 1, 2, 2, '1', '555.00', '[\"/img/prop_resale/PHOTO_URLS_3_191201_024304.jpg\"]', 1000000, 'Россия, Краснодарский край, Анапа, Советская улица ', '%7B%22ops%22%3A%5B%7B%22insert%22%3A%22%D0%A5%D0%BE%D1%80%D0%BE%D1%88%D0%B0%D1%8F%20%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D0%B0%5Cn%22%7D%5D%7D', 1, '+79282601474', NULL, '2019-12-01 14:44:02', '2022-08-31 10:51:23'),
+(7, 1, 2, 2, '3', '172.00', '[\"/img/prop_resale/PHOTO_URLS_7_220831_055646.png\"]', 4300000, 'Анапа, ул Авиагородок, 89', '%7B%22ops%22%3A%5B%7B%22insert%22%3A%22%D0%9A%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D0%B0%20%D1%83%20%D0%BC%D0%BE%D1%80%D1%8F%5Cn%22%7D%5D%7D', 2, '89180295458', 'нет', '2022-08-31 14:57:05', '2022-08-31 14:57:05');
 
 -- --------------------------------------------------------
 
@@ -89,21 +183,22 @@ INSERT INTO `NL_PROP_RESALE` (`ID_NL_PROP_RESALE`, `ID_NL_VIEW`, `ID_NL_HOUSES`,
 --
 
 CREATE TABLE `NL_USER` (
-  `ID_NL_USER` int NOT NULL,
-  `ID_NL_USER_PERMISSION` int NOT NULL,
-  `NL_USER_LOGIN` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `ID_NL_USER` int(11) NOT NULL,
+  `ID_NL_USER_PERMISSION` int(11) NOT NULL,
+  `NL_USER_LOGIN` varchar(50) NOT NULL,
   `NL_USER_PASSWORD` blob NOT NULL,
-  `NL_USER_SHORT` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
-  `NL_USER_FULL` varchar(2550) COLLATE utf8mb4_general_ci NOT NULL,
-  `NL_USER_PHONE` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `NL_USER_SHORT` varchar(25) NOT NULL,
+  `NL_USER_FULL` varchar(2550) NOT NULL,
+  `NL_USER_PHONE` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `NL_USER`
 --
 
 INSERT INTO `NL_USER` (`ID_NL_USER`, `ID_NL_USER_PERMISSION`, `NL_USER_LOGIN`, `NL_USER_PASSWORD`, `NL_USER_SHORT`, `NL_USER_FULL`, `NL_USER_PHONE`) VALUES
-(1, 2, 'admin', 0xaeb0c38af6fce07e40349cec2c17388f, 'Администратор', 'Администратор', '+79282601474');
+(1, 2, 'admin', 0xaeb0c38af6fce07e40349cec2c17388f, 'Администратор', 'Администратор', '+79282601474'),
+(2, 2, 'ddrmx', 0x3d7181e407c281f350f959b0e722af28, 'Админ', 'Администратор', '+79180295458');
 
 -- --------------------------------------------------------
 
@@ -112,10 +207,10 @@ INSERT INTO `NL_USER` (`ID_NL_USER`, `ID_NL_USER_PERMISSION`, `NL_USER_LOGIN`, `
 --
 
 CREATE TABLE `NL_USER_PERMISSION` (
-  `ID_NL_USER_PERMISSION` int NOT NULL,
-  `NL_USER_PERMISSION_SHORT` varchar(25) COLLATE utf8mb4_general_ci NOT NULL,
-  `NL_USER_PERMISSION_FULL` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID_NL_USER_PERMISSION` int(11) NOT NULL,
+  `NL_USER_PERMISSION_SHORT` varchar(25) NOT NULL,
+  `NL_USER_PERMISSION_FULL` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `NL_USER_PERMISSION`
@@ -133,28 +228,9 @@ INSERT INTO `NL_USER_PERMISSION` (`ID_NL_USER_PERMISSION`, `NL_USER_PERMISSION_S
 --
 
 CREATE TABLE `NL_VIEW` (
-  `ID_NL_VIEW` int NOT NULL,
-  `NL_VIEW_SHORT` varchar(25) COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Структура таблицы `NL_HOUSES`
---
-
-CREATE TABLE `NL_HOUSES` (
-  `ID_NL_HOUSES` int NOT NULL,
-  `ID_NL_MATERIAL` int DEFAULT NULL,
-  `NL_HOUSES_SHORT` varchar(25) COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Структура таблицы `NL_MATERIAL`
---
-
-CREATE TABLE `NL_MATERIAL` (
-  `ID_NL_MATERIAL` int NOT NULL,
-  `NL_MATERIAL_SHORT` varchar(25) COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `ID_NL_VIEW` int(11) NOT NULL,
+  `NL_VIEW_SHORT` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `NL_VIEW`
@@ -165,26 +241,15 @@ INSERT INTO `NL_VIEW` (`ID_NL_VIEW`, `NL_VIEW_SHORT`) VALUES
 (2, 'В город');
 
 --
--- Дамп данных таблицы `NL_HOUSES`
---
-
-INSERT INTO `NL_HOUSES` (`ID_NL_HOUSES`, `ID_NL_MATERIAL`, `NL_HOUSES_SHORT`) VALUES
-(1, NULL, 'Железобетонный'),
-(2, NULL, 'Блочный'),
-(3, NULL, 'Кирпичный');
-
---
--- Дамп данных таблицы `NL_MATERIAL`
---
-
-INSERT INTO `NL_MATERIAL` (`ID_NL_MATERIAL`, `NL_MATERIAL_SHORT`) VALUES
-(1, 'Панель'),
-(2, 'Газоблок'),
-(3, 'Кирпич');
-
---
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `NL_HOUSES`
+--
+ALTER TABLE `NL_HOUSES`
+  ADD PRIMARY KEY (`ID_NL_HOUSES`) USING BTREE,
+  ADD KEY `ID_NL_MATERIAL` (`ID_NL_MATERIAL`) USING BTREE;
 
 --
 -- Индексы таблицы `NL_LOG`
@@ -199,6 +264,12 @@ ALTER TABLE `NL_LOG`
 ALTER TABLE `NL_LOG_DETAIL`
   ADD PRIMARY KEY (`ID_NL_LOG_DETAIL`) USING BTREE,
   ADD KEY `ID_NL_LOG` (`ID_NL_LOG`) USING BTREE;
+
+--
+-- Индексы таблицы `NL_MATERIAL`
+--
+ALTER TABLE `NL_MATERIAL`
+  ADD PRIMARY KEY (`ID_NL_MATERIAL`) USING BTREE;
 
 --
 -- Индексы таблицы `NL_PROP_RESALE`
@@ -229,73 +300,66 @@ ALTER TABLE `NL_VIEW`
   ADD PRIMARY KEY (`ID_NL_VIEW`) USING BTREE;
 
 --
--- Индексы таблицы `NL_HOUSES`
---
-ALTER TABLE `NL_HOUSES`
-  ADD PRIMARY KEY (`ID_NL_HOUSES`) USING BTREE,
-  ADD KEY `ID_NL_MATERIAL` (`ID_NL_MATERIAL`) USING BTREE;
-
---
--- Индексы таблицы `NL_MATERIAL`
---
-ALTER TABLE `NL_MATERIAL`
-  ADD PRIMARY KEY (`ID_NL_MATERIAL`) USING BTREE;
-
---
 -- AUTO_INCREMENT для сохранённых таблиц
 --
-
---
--- AUTO_INCREMENT для таблицы `NL_LOG`
---
-ALTER TABLE `NL_LOG`
-  MODIFY `ID_NL_LOG` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `NL_LOG_DETAIL`
---
-ALTER TABLE `NL_LOG_DETAIL`
-  MODIFY `ID_NL_LOG_DETAIL` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `NL_PROP_RESALE`
---
-ALTER TABLE `NL_PROP_RESALE`
-  MODIFY `ID_NL_PROP_RESALE` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT для таблицы `NL_USER`
---
-ALTER TABLE `NL_USER`
-  MODIFY `ID_NL_USER` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT для таблицы `NL_USER_PERMISSION`
---
-ALTER TABLE `NL_USER_PERMISSION`
-  MODIFY `ID_NL_USER_PERMISSION` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT для таблицы `NL_VIEW`
---
-ALTER TABLE `NL_VIEW`
-  MODIFY `ID_NL_VIEW` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `NL_HOUSES`
 --
 ALTER TABLE `NL_HOUSES`
-  MODIFY `ID_NL_HOUSES` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID_NL_HOUSES` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `NL_LOG`
+--
+ALTER TABLE `NL_LOG`
+  MODIFY `ID_NL_LOG` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `NL_LOG_DETAIL`
+--
+ALTER TABLE `NL_LOG_DETAIL`
+  MODIFY `ID_NL_LOG_DETAIL` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT для таблицы `NL_MATERIAL`
 --
 ALTER TABLE `NL_MATERIAL`
-  MODIFY `ID_NL_MATERIAL` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID_NL_MATERIAL` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `NL_PROP_RESALE`
+--
+ALTER TABLE `NL_PROP_RESALE`
+  MODIFY `ID_NL_PROP_RESALE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT для таблицы `NL_USER`
+--
+ALTER TABLE `NL_USER`
+  MODIFY `ID_NL_USER` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `NL_USER_PERMISSION`
+--
+ALTER TABLE `NL_USER_PERMISSION`
+  MODIFY `ID_NL_USER_PERMISSION` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `NL_VIEW`
+--
+ALTER TABLE `NL_VIEW`
+  MODIFY `ID_NL_VIEW` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
+
+--
+-- Ограничения внешнего ключа таблицы `NL_HOUSES`
+--
+ALTER TABLE `NL_HOUSES`
+  ADD CONSTRAINT `nl_houses_ibfk_1` FOREIGN KEY (`ID_NL_MATERIAL`) REFERENCES `NL_MATERIAL` (`ID_NL_MATERIAL`);
 
 --
 -- Ограничения внешнего ключа таблицы `NL_LOG`
@@ -310,18 +374,12 @@ ALTER TABLE `NL_LOG_DETAIL`
   ADD CONSTRAINT `NL_LOG_DETAIL_IBFK_1` FOREIGN KEY (`ID_NL_LOG`) REFERENCES `NL_LOG` (`ID_NL_LOG`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ограничения внешнего ключа таблицы `NL_HOUSES`
---
-ALTER TABLE `NL_HOUSES` 
-  ADD CONSTRAINT `nl_houses_ibfk_1` FOREIGN KEY (`ID_NL_MATERIAL`) REFERENCES `NL_MATERIAL` (`ID_NL_MATERIAL`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
 -- Ограничения внешнего ключа таблицы `NL_PROP_RESALE`
 --
 ALTER TABLE `NL_PROP_RESALE`
-  ADD CONSTRAINT `nl_prop_resale_ibfk_1` FOREIGN KEY (`ID_NL_VIEW`) REFERENCES `NL_VIEW` (`ID_NL_VIEW`) ON DELETE RESTRICT ON UPDATE RESTRICT, 
-  ADD CONSTRAINT `nl_prop_resale_ibfk_2` FOREIGN KEY (`ID_NL_HOUSES`) REFERENCES `NL_HOUSES` (`ID_NL_HOUSES`) ON DELETE RESTRICT ON UPDATE RESTRICT, 
-  ADD CONSTRAINT `nl_prop_resale_ibfk_3` FOREIGN KEY (`ID_NL_MATERIAL`) REFERENCES `NL_MATERIAL` (`ID_NL_MATERIAL`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `nl_prop_resale_ibfk_1` FOREIGN KEY (`ID_NL_VIEW`) REFERENCES `NL_VIEW` (`ID_NL_VIEW`),
+  ADD CONSTRAINT `nl_prop_resale_ibfk_2` FOREIGN KEY (`ID_NL_HOUSES`) REFERENCES `NL_HOUSES` (`ID_NL_HOUSES`),
+  ADD CONSTRAINT `nl_prop_resale_ibfk_3` FOREIGN KEY (`ID_NL_MATERIAL`) REFERENCES `NL_MATERIAL` (`ID_NL_MATERIAL`);
 
 --
 -- Ограничения внешнего ключа таблицы `NL_USER`
